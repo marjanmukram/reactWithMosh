2c243e5aceeaab0f27d652e552d93262060e2f1b
marjanmukram <marjanmukram8@gmail.com>
2018-11-24 20:00:23 +0530

    Initials

---
+++ src/components/counter.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value
+  };
+
+  /* constructor() {
+    super();
+    this.hanldeIncrement = this.hanldeIncrement.bind(this);
+  } */
+
+  hanldeIncrement = product => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  doHanldeIncrement = () => {
+    this.hanldeIncrement({ id: 1 });
+  };
+  render() {
+    console.log("props", this.props);
+    return (
+      <div>
+        <h4>{this.props.id}</h4>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.hanldeIncrement()}
+          className="bt btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
